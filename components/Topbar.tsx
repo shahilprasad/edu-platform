992682ab90ceb66a0a978d8be446c31e2fadd7b0
shahilprasad <shahil.prasad17s@gmail.com>
2024-06-25 22:44:05 +1000

    Created routing functionality for singing in and out

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,9 +1,19 @@
+"use client"
+
 import Link from 'next/link'
 import Image from 'next/image'
 import { Search } from 'lucide-react'
 import React from 'react'
+import { UserButton, useAuth } from '@clerk/nextjs'
 
 const Topbar = () => {
+    const { isSignedIn } = useAuth()
+
+    const topRoutes = [
+        { label: "Instructor", path: "/instructor/courses" },
+        { label: "Learning", path: "/learning" }
+    ]
+
   return (
     <div className="flex justify-between items-center p-4">
         <Link href="/">
@@ -18,6 +28,24 @@ const Topbar = () => {
             <button className="bg-[#FDAB04] rounded-r-full border-none outline-none cursor-pointer px-4 py-3 hover:bg-[#FDAB04]/80">
                 <Search className="h-4 w-4" />
             </button>
+        </div>
+
+        <div className="flex gap-6 item-center">
+            <div className="max-sm:hidden flex gap-6">
+                {topRoutes.map((route) => (
+                    <Link 
+                    href={route.path} 
+                    key={route.path} 
+                    className="text-sm font-medium hover:text-[#FDAB04]">
+                    {route.label}
+                    </Link>
+                ))}
+            </div>
+            {isSignedIn ? (
+                <UserButton afterSignOutUrl="/sign-in" />
+            ) : (
+                <Link href="/sign-in">Sign In</Link>
+            )} 
         </div>
     </div>
   )
